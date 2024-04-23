51cc5806feb9eaabe7579a88306e653a3d00d186
user <user@users-MacBook-Air-2.local>
2024-04-23 10:22:44 -0700

    Checkpoint 1

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,10 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = { 5, 9, 3, 2, 6 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 6, 2, 3, 9, 5 }, input2);
 	}
 
 
@@ -14,5 +18,9 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = { 5, 9, 3, 2, 6 };
+    ArrayExamples.reversed(input2);
+    assertArrayEquals(new int[]{ 6, 2, 3, 9, 5 }, ArrayExamples.reversed(input2));
   }
 }
